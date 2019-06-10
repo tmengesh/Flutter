fbc3d98aa6e2ee1b606754172687b7154c320b60
Tewodros Mengesha <tewodros.mengesha@finwe.fi>
2019-06-10 12:54:13 +0300

    Update main.dart

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -6,6 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
